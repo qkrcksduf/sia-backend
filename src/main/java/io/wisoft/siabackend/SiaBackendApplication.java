faf8bf23bc5860b31c5e693f6f64bc0df3b4a1cf
Chanyeol Park <chanyeol@edu.hanbat.ac.kr>
2021-06-15 16:58:05 +0900

    Initial Setting

---
+++ src/main/java/io/wisoft/siabackend/SiaBackendApplication.java
@@ -0,0 +1,13 @@
+package io.wisoft.siabackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiaBackendApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(SiaBackendApplication.class, args);
+  }
+
+}
---
+++ src/test/java/io/wisoft/siabackend/SiaBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package io.wisoft.siabackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiaBackendApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
